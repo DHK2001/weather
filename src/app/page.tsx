43902fe27bf7e4fa595af82a2ad0955377b20c94
Derek Galeas <dhenrygk12@gmail.com>
2025-01-10 13:12:12 -0600

    remove imports that are not necessary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { fetchWeatherByCity } from "@/services/api";
-import { Weather, WeatherData } from "@/services/weather-interfaces";
+import { Weather } from "@/services/weather-interfaces";
 import { cities, getDate, getIcons, groupWeatherData } from "@/utils/helpers";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState, useEffect } from "react";
-import { Suspense } from "react";
 
 export default function Home() {
   const [showMore, setShowMore] = useState<number | null>(null);
